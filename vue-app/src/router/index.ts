41692ae67a3256623afb4d69ae0fb4ddcb744cf1
bdoggy4pf <bjarnidagur77@gmail.com>
2025-09-17 15:29:10 +0000

    bætti við NotFound

--- vue-app/src/router/index.ts
+++ vue-app/src/router/index.ts
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HelloWorld from '../components/HelloWorld.vue'
-import TheWelcome from '../components/TheWelcome.vue' 
+import TheWelcome from '../components/TheWelcome.vue'
+import NotFound from '../components/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HelloWorld },
-  { path: '/about', name: 'about', component: TheWelcome }, 
-  { path: '/:pathMatch(.*)*', name: 'not-found', component: HelloWorld } //hálfgert 404? vantar meira... 
+  { path: '/about', name: 'about', component: TheWelcome },
+  { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound } // almenn 404 síða
 ]
 
 export default createRouter({
