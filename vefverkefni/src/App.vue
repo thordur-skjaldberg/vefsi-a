47450d4f7627d747d2e1dd18b15d4a1c4f77e861
Thordur Skjaldberg <1tordur@gmail.com>
2025-09-18 13:47:32 +0000

    naði lokins að breyta html skram i vue

---
+++ vefverkefni/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <header>
+      <h1>Innihald</h1>
+    </header>
+
+    <main>
+      <div class="camera-placeholder">
+        <p>Skannaðu hér</p>
+      </div>
+
+      <div class="info">
+        <p>Beindu símanum að strikamerki eða innihaldslýsingu.</p>
+      </div>
+    </main>
+
+    <footer>
+      <button class="btn">Leit</button>
+    </footer>
+
+ 
+    <nav class="fimmkassar">
+      <a href="kort.html">Kort</a>
+      <a href="samanburður.html">Samanburður</a>
+      <a href="forsida.html" class="active">Skanna</a>
+      <a href="user.html">Notandi</a>
+      <a href="baetaviðvoru.html">Meira</a>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+</style>
